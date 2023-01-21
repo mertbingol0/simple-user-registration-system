6299a78ccc3a03a4c0360374397f982aec0ca379
Mert Bingol <mertbingol00@protonmail.com>
2023-01-22 01:26:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template, request
-from users_db import create_db
+from users_db import create_db, password_hashing
 
 app = Flask(__name__)
 
-# Endpoint
+#Endpoint
 @app.route('/register', methods = ['POST', 'GET'])
 def register():
    
-    if request.method == 'POST': 
-        # Here, it sends the data it captures with get to the create_db function.
-        create_db(request.form.get('username'), request.form.get('password')) 
-        # Run html file with render_template.
-        return render_template('register.html')
+    if request.method == 'POST':
+      
+        #Here, it sends the data it captures with get to the create_db function.
+        username = request.form.get('username')
+        password = request.form.get('password')
+        hashed_password = password_hashing(password)
+        create_db(username, hashed_password)
+      
+        return "User Created"
     else:
-        return render_template('Server Side Error')
+        return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
