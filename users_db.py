bd8e2fc954255f1a54c6c2f1fb8966cddfe2f5e2
Mert Bingol <mertbingol00@protonmail.com>
2023-01-22 01:27:26 +0300

    Update users_db.py

--- users_db.py
+++ users_db.py
@@ -1,7 +1,13 @@
 import sqlite3
+import hashlib
+
+#Hashing password
+def password_hashing(password):
+    password = hashlib.sha256(password.encode()).hexdigest()
+    return password
 
 #Create users
-def create_db(username, password):
+def create_db(username, hashed_password):
     #Classic sqllite operations.
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
@@ -12,5 +18,5 @@ def create_db(username, password):
 
     cursor.execute(create_table)
     #Here we add the username and password values ​​that we have obtained from the register.html file to the db.
-    cursor.execute(input_users, (username, password))
+    cursor.execute(input_users, (username, hashed_password))
     conn.commit()
